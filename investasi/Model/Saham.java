70f36914166292de86f0474ef4559e0585bacb48
mrdawn656 <fajarharryputra@gmail.com>
2025-04-21 19:40:21 +0800

    up saham.java

--- investasi/Model/Saham.java
+++ investasi/Model/Saham.java
@@ -1,4 +1,41 @@
 package Model;
 
+import java.util.Objects;
+
 public class Saham {
+    private String kode;
+    private String namaPerusahaan;
+    private double harga;
+
+    public Saham(String kode, String namaPerusahaan, double harga) {
+        this.kode = kode;
+        this.namaPerusahaan = namaPerusahaan;
+        this.harga = harga;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+
+    public String getNamaPerusahaan() {
+        return namaPerusahaan;
+    }
+
+    public void setNamaPerusahaan(String namaPerusahaan) {
+        this.namaPerusahaan = namaPerusahaan;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
 }
+
