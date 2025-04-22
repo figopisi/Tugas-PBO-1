873b4fc1aa6cfd9af3a13113571d61f6a19cecd1
Figo Laksmana <figodh117@gmail.com>
2025-04-23 01:28:52 +0800

    creating the model of SBM

---
+++ investasi/Model/SuratBerhargaNegara.java
@@ -0,0 +1,65 @@
+package Model;
+
+public class SuratBerhargaNegara {
+    private String name;
+    private double interestRate;
+    private int duration; // in months
+    private String maturityDate;
+    private int nationalQuota;
+
+    public SuratBerhargaNegara(String name, double interestRate, int duration, String maturityDate, int nationalQuota) {
+        this.name = name;
+        this.interestRate = interestRate;
+        this.duration = duration;
+        this.maturityDate = maturityDate;
+        this.nationalQuota = nationalQuota;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getInterestRate() {
+        return interestRate;
+    }
+
+    public void setInterestRate(double interestRate) {
+        this.interestRate = interestRate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public String getMaturityDate() {
+        return maturityDate;
+    }
+
+    public void setMaturityDate(String maturityDate) {
+        this.maturityDate = maturityDate;
+    }
+
+    public int getNationalQuota() {
+        return nationalQuota;
+    }
+
+    public void setNationalQuota(int nationalQuota) {
+        this.nationalQuota = nationalQuota;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + name + ", Interest Rate: " + interestRate + "%" +
+                ", Duration: " + duration + " months" +
+                ", Maturity Date: " + maturityDate +
+                ", National Quota: " + nationalQuota;
+    }
+}
