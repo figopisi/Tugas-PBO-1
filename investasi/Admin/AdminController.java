5baf5bb17f6b467670b3171307d7ee0d704ebaa3
mrdawn656 <fajarharryputra@gmail.com>
2025-04-27 19:21:57 +0800

    update AdminController.java

--- investasi/Admin/AdminController.java
+++ investasi/Admin/AdminController.java
@@ -1,33 +1,43 @@
 package Admin;
 
-import Model.Saham;
-import Model.SuratBerhargaNegara;
-import Service.SahamService;
-import Service.SBNService;
-import Util.InputHelper;
+import View.Admin.AdminSBN;
+import View.Admin.AdminSaham;
+import util.InputHelper;
 
 public class AdminController {
-    private final SahamService sahamSvc;
-    private final SBNService sbnSvc;
+    private AdminSBN adminSBN;
+    private AdminSaham adminSaham;
 
-    public AdminController(SahamService sahamSvc, SBNService sbnSvc) {
-        this.sahamSvc = sahamSvc;
-        this.sbnSvc   = sbnSvc;
+    public AdminController() {
+        adminSBN = new AdminSBN();
+        adminSaham = new AdminSaham();
     }
 
-    public void menu() {
-        int pilihan;
+    public void adminMenu() {
+        int choice;
         do {
-            System.out.println("\n--- Menu Admin ---");
-            System.out.println("1. Saham");
-            System.out.println("2. SBN");
-            System.out.println("3. Logout");
-            pilihan = InputHelper.readInt("Pilih: ", 1, 3);
+            System.out.println("\n============================================");
+            System.out.println("|             ADMIN MENU                   |");
+            System.out.println("|==========================================|");
+            System.out.println("| 1. Kelola SBN                            |");
+            System.out.println("| 2. Kelola Saham                          |");
+            System.out.println("| 3. Logout                                |");
+            System.out.println("============================================");
+            choice = InputHelper.readInt("Pilih menu [1-3]: ", 1, 3);
 
-            switch (pilihan) {
-                case 1: menuSaham(); break;
-                case 2: menuSBN(); break;
-                case 3: System.out.println("Logout Admin."); break;
+            switch (choice) {
+                case 1:
+                    adminSBN.adminSBNMenu();
+                    break;
+                case 2:
+                    adminSaham.adminSahamMenu();
+                    break;
+                case 3:
+                    System.out.println("Logout dari akun Admin...");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi.");
             }
-        } while (pilihan != 3);
+        } while (choice != 3);
     }
+}
