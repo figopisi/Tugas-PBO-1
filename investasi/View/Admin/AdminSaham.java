75997b4ff6811e1f113518b7c599bf42bb2fd677
Figo Laksmana <figodh117@gmail.com>
2025-04-28 03:31:01 +0800

    syncronize admin saham menu

--- investasi/View/Admin/AdminSaham.java
+++ investasi/View/Admin/AdminSaham.java
@@ -1,40 +1,40 @@
 package View.Admin;
 
-import Service.SBN.addSBN;
+import Service.SahamService;
 import Util.InputHelper;
 
-
 public class AdminSaham {
-    private addSBN sbnService;
+    private final SahamService sahamService;
 
-    public AdminSaham() {
-            sbnService = new addSBN();
+    public AdminSaham(SahamService sahamService) {
+        this.sahamService = sahamService;
     }
 
-    public void adminSBNMenu() {
+    public void menu() {
         int choice;
         do {
             System.out.println("\n============================================");
-            System.out.println("|              Saham MENU                     |");
+            System.out.println("|              SAHAM MENU                  |");
             System.out.println("|===========================================|");
-            System.out.println("| 1. Tambah Saham                             |");
-            System.out.println("| 2. Ubah Harga Saham                           |");
-            System.out.println("| 3. Kembali                                |");
+            System.out.println("| 1. Tambah Saham                          |");
+            System.out.println("| 2. Ubah Harga Saham                      |");
+            System.out.println("| 3. Kembali                               |");
             System.out.println("============================================");
-            choice = InputHelper.readInt("Pilih menu [1-2]: ", 1, 2);
+            choice = InputHelper.readInt("Pilih menu [1-3]: ", 1, 3);
 
             switch (choice) {
-                    case 1:
-                        sbnService.addSBN();
-                        break;
-                    case 2:
-                        System.out.println("Kembali ke menu Admin.");
-                        break;
-                    default:
-                        System.out.println("Pilihan tidak valid. Coba lagi.");
+                case 1:
+                    sahamService.addSaham();
+                    break;
+                case 2:
+                    sahamService.updatePrice();
+                    break;
+                case 3:
+                    System.out.println("Kembali ke menu Admin.");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Coba lagi.");
             }
-        } while (choice != 2);
+        } while (choice != 4);
     }
 }
-
-
