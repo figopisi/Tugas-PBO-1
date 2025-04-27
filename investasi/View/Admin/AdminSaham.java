205f9a98f8f39b6c4a616cb7478375e677cc10ce
Figo Laksmana <figodh117@gmail.com>
2025-04-27 15:47:04 +0800

    Make a menu for saham

---
+++ investasi/View/Admin/AdminSaham.java
@@ -0,0 +1,41 @@
+package View.Admin;
+
+import Service.SBN.addSBN;
+import util.InputHelper;
+
+public class AdminSaham {
+
+    public class Adminsaham {
+        private addSBN sbnService;
+
+        public Adminsaham() {
+            sbnService = new addSBN();
+        }
+
+        public void adminSBNMenu() {
+            int choice;
+            do {
+                System.out.println("\n============================================");
+                System.out.println("|              Saham MENU                     |");
+                System.out.println("|===========================================|");
+                System.out.println("| 1. Tambah Saham                             |");
+                System.out.println("| 2. Ubah Harga Saham                           |");
+                System.out.println("| 3. Kembali                                |");
+                System.out.println("============================================");
+                choice = InputHelper.readInt("Pilih menu [1-2]: ", 1, 2);
+
+                switch (choice) {
+                    case 1:
+                        sbnService.addSBN();
+                        break;
+                    case 2:
+                        System.out.println("Kembali ke menu Admin.");
+                        break;
+                    default:
+                        System.out.println("Pilihan tidak valid. Coba lagi.");
+                }
+            } while (choice != 2);
+        }
+    }
+
+}
